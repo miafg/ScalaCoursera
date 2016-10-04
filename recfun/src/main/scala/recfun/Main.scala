e79037bc31c7121edee2fae0af610ba3d22e1bb2
miafg <miaf@sas.upenn.edu>
2016-10-04 11:29:28 -0400

    first commit

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,36 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if (c < 0 || c > r + 1) {
+        return 0;
+      }
+      if (c == 0 || c == r) {
+        return 1;
+      }
+      return pascal(c - 1, r - 1) + pascal(c, r - 1);
+    }
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = {
+
+    }
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
